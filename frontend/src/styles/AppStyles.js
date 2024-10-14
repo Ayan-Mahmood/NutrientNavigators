6b49b5dd034334260b794ab70275914aa1eea110
Ayan Mahmood <ayan@mahmood.com>
2024-10-14 11:14:58 -0400

    Worked with team to develop basic react native frontend

---
+++ frontend/src/styles/AppStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const AppStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  text: {
+    fontSize: 20,
+    color: '#333',
+  },
+});
+
+export default AppStyles;
